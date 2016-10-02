5020998909965efc4429c14de294d0d64e515d53
dapeng126 <zhangdapeng89@126.com>
2016-10-02 15:17:44 +0800

    给图片下载添加进度条

--- app/src/main/java/com/gowhich/androidasynctask/MainActivity.java
+++ app/src/main/java/com/gowhich/androidasynctask/MainActivity.java
@@ -17,6 +17,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpCookie;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("下载提示");
         progressDialog.setMessage("图片下载中.....");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
      * 1,声明一个类继承AsyncTask, 标注三个参数的类型
      * 2,第一个参数表示要执行的任务通常是网络的路径,第二个参数表示进度的刻度,第三个参数表示任务执行的返回结果
      */
-    public class MyTask extends AsyncTask<String, Void, Bitmap>{
+    public class MyTask extends AsyncTask<String, Integer, Bitmap> {
 
         @Override
         protected void onPreExecute() {
@@ -70,20 +74,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressDialog.setProgress(values[0]);
+        }
+
+        @Override
         protected Bitmap doInBackground(String... params) {
             HttpClient httpClient = new DefaultHttpClient();
             HttpGet httpGet = new HttpGet(params[0]);
             Bitmap bitmap = null;
-            try{
-                HttpResponse httpResponse = httpClient.execute(httpGet);
-                if(httpResponse.getStatusLine().getStatusCode() == 200){
-                    HttpEntity httpEntity = httpResponse.getEntity();
 
-                    byte[] data = EntityUtils.toByteArray(httpEntity);
-                    bitmap  = BitmapFactory.decodeByteArray(data, 0, data.length);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            InputStream inputStream = null;
+
+
+            try {
+                HttpResponse httpResponse = httpClient.execute(httpGet);
+                if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                    inputStream = httpResponse.getEntity().getContent();
+                    long fileLength = httpResponse.getEntity().getContentLength();
+                    int len = 0;
+                    byte[] data = new byte[2024];
+                    int totalLength = 0;
+                    while ((len = inputStream.read(data)) != -1) {
+                        totalLength += len;
+                        int value = (int) ((totalLength / (float) fileLength) * 100);
+                        publishProgress(value);
+                        outputStream.write(data, 0, len);
+                    }
+
+                    byte[] result = outputStream.toByteArray();
+                    bitmap = BitmapFactory.decodeByteArray(result, 0, result.length);
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
             return bitmap;
         }
